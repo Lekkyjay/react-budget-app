dd0f5f7fea8cb2f1b473147d6c0111a5904a0e66
Lekkyjay <lekkyjayjay@yahoo.com>
2020-06-29 15:14:24 +0200

    App completed with browser localStorage functionality

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,24 +1,29 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect  } from "react";
 import AppReducer from './AppReducer'
 
 //create an initial state
 const initialState = {
-  incomeTransactions: [
-    {id: 1, incomeText: "Car sold", incomeAmount: 1500},
-    {id: 2, incomeText: "Salary", incomeAmount: 5000},
-    {id: 3, incomeText: "Bonus", incomeAmount: 1300}
-  ],
-  expenseTransactions: [
-    {id: 4, expenseText: "Rent", expenseAmount: 1000},
-    {id: 5, expenseText: "Bank", expenseAmount: 2000},
-    {id: 6, expenseText: "Clothes", expenseAmount: 500}
-  ]
-}
+  incomeTransactions:
+    JSON.parse(localStorage.getItem("incomeTransactions")) || [],
+  expenseTransactions:
+    JSON.parse(localStorage.getItem("expenseTransactions")) || []
+};
 
 export const GlobalContext = createContext(initialState);
 
 export const GlobalContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem(
+      "incomeTransactions",
+      JSON.stringify(state.incomeTransactions)
+    );
+    localStorage.setItem(
+      "expenseTransactions",
+      JSON.stringify(state.expenseTransactions)
+    );
+  });
 
   const addIncome = incomeTransaction => {
     dispatch({
