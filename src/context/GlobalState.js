ecfe6c549ad89893f0f1a5a85c8db3f1c11639a7
Lekkyjay <lekkyjayjay@yahoo.com>
2020-06-29 15:05:18 +0200

    App completed with hardcoded initialState data

---
+++ src/context/GlobalState.js
@@ -0,0 +1,57 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from './AppReducer'
+
+//create an initial state
+const initialState = {
+  incomeTransactions: [
+    {id: 1, incomeText: "Car sold", incomeAmount: 1500},
+    {id: 2, incomeText: "Salary", incomeAmount: 5000},
+    {id: 3, incomeText: "Bonus", incomeAmount: 1300}
+  ],
+  expenseTransactions: [
+    {id: 4, expenseText: "Rent", expenseAmount: 1000},
+    {id: 5, expenseText: "Bank", expenseAmount: 2000},
+    {id: 6, expenseText: "Clothes", expenseAmount: 500}
+  ]
+}
+
+export const GlobalContext = createContext(initialState);
+
+export const GlobalContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  const addIncome = incomeTransaction => {
+    dispatch({
+      type: "ADD_INCOME",
+      payload: incomeTransaction
+    });
+  };
+
+  const addExpense = expenseTransaction => {
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expenseTransaction
+    });
+  };
+
+  const deleteTransaction = id => {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id
+    });
+  };
+
+  return (
+    <GlobalContext.Provider 
+      value={{
+        incomeTransactions: state.incomeTransactions,
+        expenseTransactions: state.expenseTransactions,
+        deleteTransaction,
+        addIncome,
+        addExpense
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  )
+}
